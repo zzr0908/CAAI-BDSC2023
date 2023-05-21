6e33257b8642f6f1c3793bb999b3087f373adb8b
KIKI11111 <1487793439@qq.com>
2023-05-21 22:54:19 +0800

    shell 运行

---
+++ run.sh
@@ -0,0 +1,8 @@
+
+python main.py \
+--pretrain_model 'Sage' --finetune_model 'Sage' --device 'cpu' \
+--source_val_frac 0.1 --rs 2023 \
+--pretrain_embedding 256 --pretrain_hidden_feats [256,256] --pretrain_batch_size 2048 --pretrain_epoch 10 \
+--pretrain_loss multi_label_loss --pretrain_sample_neighbor [-1,-1] \
+--finetune_epoch 10 --finetune_loss multi_label_loss --finetune_batch_size 1024 \
+--recall_level 2 --min_recall 10
