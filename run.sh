99108f8d81e1c6b927dd7260c08cb2851927b624
KIKI11111 <1487793439@qq.com>
2023-05-21 23:05:50 +0800

    shell 运行

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
 python main.py \
---pretrain_model 'Sage' --finetune_model 'Sage' --device 'cpu' \
+--pretrain_model 'Sage' --finetune_model 'Sage' --device 'cuda:0' \
 --source_val_frac 0.1 --rs 2023 \
 --pretrain_embedding 256 --pretrain_hidden_feats [256,256] --pretrain_batch_size 2048 --pretrain_epoch 10 \
 --pretrain_sample_neighbor [-1,-1] \
